86f0b016ff6d822c82a7f4389e978be2d4b8bcd8
Шефер Артём Евгеньевич <shefer_ae@irgups.ru>
2026-04-01 16:14:41 +0800

    last task

--- tusur_task2/dist/scripts/main.js
+++ tusur_task2/dist/scripts/main.js
@@ -277,6 +277,133 @@
                 title: "ИТ-Академия ТУСУР"
             });
         }
+        const jsSelectric = $(".js-selectric");
+
+        if (jsSelectric.length) {
+            jsSelectric.selectric({
+                nativeOnMobile: false
+            });
+        }
+
+        const mobileMask = $('.js-mobileMask');
+
+        if (mobileMask.length) {
+            mobileMask.mask('+7 (000) 000 00 00', { placeholder: "+7 (___) ___ __ __" });
+        }
+
+        // Инициализация календаря даты
+        const dateField = $(".js-dateField");
+
+        if (dateField.length) {
+            const pickerInit = function (pick) {
+                const dateInput = pick.find(".js-dateInput");
+                const dateDay = pick.find(".js-dateDay");
+                const dateMonth = pick.find(".js-dateMonth");
+                const dateYear = pick.find(".js-dateYear");
+
+                const dateConfig = {
+                    autoClose: true,
+                    minDate: new Date(),
+                    navTitles: {
+                        days: "MMMM <i>yyyy</i>"
+                    },
+                    onSelect: function ({ date }) {
+                        dateDay.val(date ? ("0" + date.getDate()).slice(-2) : "");
+                        dateMonth.val(date ? ("0" + (date.getMonth() + 1)).slice(-2) : "");
+                        dateYear.val(date ? date.getFullYear() : "");
+                    }
+                };
+                new AirDatepicker(dateInput[0], dateConfig);
+            };
+
+            $.each(dateField, function (i) {
+                pickerInit($(this));
+            });
+        }
+
+        (function () {
+            if (!$ || !$.validator) return;
+
+            $.extend($.validator.messages, {
+                required: "Это поле обязательно",
+                remote: "Пожалуйста, введите правильное значение.",
+                email: "Пожалуйста, введите корректный e-mail",
+                url: "Пожалуйста, введите корректный URL.",
+                date: "Пожалуйста, введите корректную дату.",
+                dateISO: "Пожалуйста, введите корректную дату в формате ISO.",
+                number: "Пожалуйста, введите число.",
+                digits: "Пожалуйста, вводите только цифры.",
+                creditcard: "Пожалуйста, введите правильный номер кредитной карты.",
+                equalTo: "Пароли не совпадают!",
+                extension: "Пожалуйста, выберите файл с правильным расширением.",
+                maxlength: $.validator.format(
+                    "Пожалуйста, введите не больше {0} символов."
+                ),
+                minlength: $.validator.format(
+                    "Пожалуйста, введите не меньше {0} символов."
+                ),
+                rangelength: $.validator.format(
+                    "Пожалуйста, введите значение длиной от {0} до {1} символов."
+                ),
+                range: $.validator.format("Пожалуйста, введите число от {0} до {1}."),
+                max: $.validator.format(
+                    "Пожалуйста, введите число, меньшее или равное {0}."
+                ),
+                min: $.validator.format(
+                    "Пожалуйста, введите число, большее или равное {0}."
+                )
+            });
+
+            $.validator.addMethod(
+                "email",
+                function (value, element) {
+                    return (
+                        this.optional(element) ||
+                        /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,6}$/.test(value)
+                    );
+                },
+                "Введите корректный e-mail"
+            );
+
+            const eventForm = $('#js-eventForm');
+
+            if (eventForm.length) {
+                eventForm.validate({
+                    errorElement: "span"
+                });
+            }
+        })();
+
+        const subscribeForm = $("#js-subscribeForm");
+
+        if (subscribeForm.length) {
+
+            const subscribeAction = subscribeForm.attr("action");
+            const subscribeEmail = subscribeForm.find("#js-subscribeEmail");
+
+            subscribeForm.validate({
+                errorElement: "span",
+                submitHandler: function (form, event) {
+                    event.preventDefault();
+
+                    $.ajax({
+                        url: subscribeAction,
+                        method: "POST",
+                        data: {
+                            email: subscribeEmail.val()
+                        },
+                        success: function () {
+                            subscribeEmail.val("");
+                            subscribeEmail.blur();
+                            alert("Вы успешно подписались на рассылку новостей");
+                        },
+                        error: function () {
+                            alert("Что-то пошло не так, попробуйте еще раз");
+                        }
+                    });
+                }
+            });
+        }
     });
 })();
 
